a8207ef5b8d07b707ec468346f2c9310ebd2575d
Wasif Khan <waasif.khaan@gmail.com>
2018-08-13 22:30:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,18 @@
 class Student
+attr_accessor :name, :grade
+
+def initialize(name:, grade:, id:)
+  @id = id 
+  @name = name
+  @grade = grade 
+end 
+
+def self.create
+  sql = <<-SQL 
+  CREATE TABLE 
+  
+  SQL 
+end 
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
