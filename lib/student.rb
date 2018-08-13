0149edf6a4e66b07639ad0be91e4b1eb76357e7c
Wasif Khan <waasif.khaan@gmail.com>
2018-08-13 22:46:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,11 +24,10 @@ def self.drop_table
 end 
 
 def save 
-  sql = <<-SQL
-  INSERT INTO students( 
-  name 
-  
-  )
+  insert = <<-SQL
+  INSERT INTO students(name, grade) VALUES ( ?, ?)  
+  SQL
+  DB[:conn].execute(insert,self.name, self.grade)
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
