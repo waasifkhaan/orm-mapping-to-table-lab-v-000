8a7541045f275d620d477881b1797be2199e6ca0
Wasif Khan <waasif.khaan@gmail.com>
2018-08-13 22:43:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,8 +12,7 @@ def self.create_table
   CREATE TABLE IF NOT EXISTS students (
   id INTEGER PRIMARY KEY, 
   name TEXT, 
-  grade INTEGER
-  )
+  grade INTEGER)
   SQL
   DB[:conn].execute(sql)
 end 
